2d40a0655e7bf1dde576bc5382d1923b7611db60
Camilo Figueroa <cigotete@gmail.com>
2024-06-04 12:41:16 -0500

    Implementing nested chat to add a team of agents to help Critic agent to criticize writer text proposed.

--- main.py
+++ main.py
@@ -19,10 +19,6 @@ writer = autogen.AssistantAgent(
     llm_config=llm_config,
 )
 
-#reply = writer.generate_reply(messages=[{"content": task, "role": "user"}])
-
-#print(reply)
-
 critic = autogen.AssistantAgent(
     name="Critic",
     is_termination_msg=lambda x: x.get("content", "").find("TERMINATE") >= 0,
@@ -30,6 +26,82 @@ critic = autogen.AssistantAgent(
     system_message="You are a critic. You review the work of "
                 "the writer and provide constructive "
                 "feedback to help improve the quality of the content.",
+)
+
+seo_reviewer = autogen.AssistantAgent(
+    name="SEO Reviewer",
+    llm_config=llm_config,
+    system_message="You are an SEO reviewer, known for "
+        "your ability to optimize content for search engines, "
+        "ensuring that it ranks well and attracts organic traffic. " 
+        "Make sure your suggestion is concise (within 3 bullet points), "
+        "concrete and to the point. "
+        "Begin the review by stating your role.",
+)
+
+legal_reviewer = autogen.AssistantAgent(
+    name="Legal Reviewer",
+    llm_config=llm_config,
+    system_message="You are a legal reviewer, known for "
+        "your ability to ensure that content is legally compliant "
+        "and free from any potential legal issues. "
+        "Make sure your suggestion is concise (within 3 bullet points), "
+        "concrete and to the point. "
+        "Begin the review by stating your role.",
+)
+
+ethics_reviewer = autogen.AssistantAgent(
+    name="Ethics Reviewer",
+    llm_config=llm_config,
+    system_message="You are an ethics reviewer, known for "
+        "your ability to ensure that content is ethically sound "
+        "and free from any potential ethical issues. " 
+        "Make sure your suggestion is concise (within 3 bullet points), "
+        "concrete and to the point. "
+        "Begin the review by stating your role. ",
+)
+
+meta_reviewer = autogen.AssistantAgent(
+    name="Meta Reviewer",
+    llm_config=llm_config,
+    system_message="You are a meta reviewer, you aggragate and review "
+    "the work of other reviewers and give a final suggestion on the content.",
+)
+
+def reflection_message(recipient, messages, sender, config):
+    return f'''Review the following content. 
+            \n\n {recipient.chat_messages_for_summary(sender)[-1]['content']}'''
+
+review_chats = [
+    {
+     "recipient": seo_reviewer,
+     "message": reflection_message,
+     "summary_method": "reflection_with_llm",
+     "summary_args": {"summary_prompt" :
+        "Return review into as JSON object only:"
+        "{'Reviewer': '', 'Review': ''}. Here Reviewer should be your role",},
+     "max_turns": 1},
+    {
+    "recipient": legal_reviewer, "message": reflection_message,
+     "summary_method": "reflection_with_llm",
+     "summary_args": {"summary_prompt" :
+        "Return review into as JSON object only:"
+        "{'Reviewer': '', 'Review': ''}.",},
+     "max_turns": 1},
+    {"recipient": ethics_reviewer, "message": reflection_message, 
+     "summary_method": "reflection_with_llm",
+     "summary_args": {"summary_prompt" :
+        "Return review into as JSON object only:"
+        "{'reviewer': '', 'review': ''}",},
+     "max_turns": 1},
+     {"recipient": meta_reviewer,
+      "message": "Aggregrate feedback from all reviewers and give final suggestions on the writing.",
+     "max_turns": 1},
+]
+
+critic.register_nested_chats(
+    review_chats,
+    trigger=writer,
 )
 
 res = critic.initiate_chat(
