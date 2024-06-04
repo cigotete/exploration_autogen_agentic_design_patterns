cbb6431acc7a4cacabbab313cbad19c629c8cbc2
Camilo Figueroa <cigotete@gmail.com>
2024-06-04 15:19:12 -0500

    Added agents to play and board proxy agent.

--- main.py
+++ main.py
@@ -42,3 +42,39 @@ def make_move(
     return f"Moved {piece_name} ({piece_symbol}) from "\
     f"{chess.SQUARE_NAMES[move.from_square]} to "\
     f"{chess.SQUARE_NAMES[move.to_square]}."
+
+from autogen import ConversableAgent
+
+# Player white agent
+player_white = ConversableAgent(
+    name="Player White",
+    system_message="You are a chess player and you play as white. "
+    "First call get_legal_moves(), to get a list of legal moves. "
+    "Then call make_move(move) to make a move.",
+    llm_config=llm_config,
+)
+
+# Player black agent
+player_black = ConversableAgent(
+    name="Player Black",
+    system_message="You are a chess player and you play as black. "
+    "First call get_legal_moves(), to get a list of legal moves. "
+    "Then call make_move(move) to make a move.",
+    llm_config=llm_config,
+)
+
+def check_made_move(msg):
+    global made_move
+    if made_move:
+        made_move = False
+        return True
+    else:
+        return False
+    
+board_proxy = ConversableAgent(
+    name="Board Proxy",
+    llm_config=False,
+    is_termination_msg=check_made_move,
+    default_auto_reply="Please make a move.",
+    human_input_mode="NEVER",
+)
