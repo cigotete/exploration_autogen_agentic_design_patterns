1372bd71c3395bcb1dd887012e480a1d2051c5b9
Camilo Figueroa <cigotete@gmail.com>
2024-06-04 15:17:19 -0500

    A chess game based with Agents. Added functions to get moves, and to move.

--- main.py
+++ main.py
@@ -2,111 +2,43 @@ from utils import get_openai_api_key
 OPENAI_API_KEY = get_openai_api_key()
 llm_config = {"model": "gpt-3.5-turbo"}
 
-task = '''
-        Write a concise but engaging blogpost about
-       DeepLearning.AI. Make sure the blogpost is
-       within 100 words.
-       '''
+import chess
+import chess.svg
+from typing_extensions import Annotated
 
-import autogen
+board = chess.Board()
+made_move = False
 
-writer = autogen.AssistantAgent(
-    name="Writer",
-    system_message="You are a writer. You write engaging and concise " 
-        "blogpost (with title) on given topics. You must polish your "
-        "writing based on the feedback you receive and give a refined "
-        "version. Only return your final work without additional comments.",
-    llm_config=llm_config,
-)
+def get_legal_moves(
+) -> Annotated[str, "A list of legal moves in UCI format"]:
+    return "Possible moves are: " + ",".join(
+        [str(move) for move in board.legal_moves]
+    )
 
-critic = autogen.AssistantAgent(
-    name="Critic",
-    is_termination_msg=lambda x: x.get("content", "").find("TERMINATE") >= 0,
-    llm_config=llm_config,
-    system_message="You are a critic. You review the work of "
-                "the writer and provide constructive "
-                "feedback to help improve the quality of the content.",
-)
+def make_move(
+    move: Annotated[str, "A move in UCI format."]
+) -> Annotated[str, "Result of the move."]:
+    move = chess.Move.from_uci(move)
+    board.push_uci(str(move))
+    global made_move
+    made_move = True
 
-seo_reviewer = autogen.AssistantAgent(
-    name="SEO Reviewer",
-    llm_config=llm_config,
-    system_message="You are an SEO reviewer, known for "
-        "your ability to optimize content for search engines, "
-        "ensuring that it ranks well and attracts organic traffic. " 
-        "Make sure your suggestion is concise (within 3 bullet points), "
-        "concrete and to the point. "
-        "Begin the review by stating your role.",
-)
+    # Display the board.
+    chess.svg.board(
+        board,
+        arrows=[(move.from_square, move.to_square)],
+        fill={move.from_square: "gray"},
+        size=200
+    )
 
-legal_reviewer = autogen.AssistantAgent(
-    name="Legal Reviewer",
-    llm_config=llm_config,
-    system_message="You are a legal reviewer, known for "
-        "your ability to ensure that content is legally compliant "
-        "and free from any potential legal issues. "
-        "Make sure your suggestion is concise (within 3 bullet points), "
-        "concrete and to the point. "
-        "Begin the review by stating your role.",
-)
-
-ethics_reviewer = autogen.AssistantAgent(
-    name="Ethics Reviewer",
-    llm_config=llm_config,
-    system_message="You are an ethics reviewer, known for "
-        "your ability to ensure that content is ethically sound "
-        "and free from any potential ethical issues. " 
-        "Make sure your suggestion is concise (within 3 bullet points), "
-        "concrete and to the point. "
-        "Begin the review by stating your role. ",
-)
-
-meta_reviewer = autogen.AssistantAgent(
-    name="Meta Reviewer",
-    llm_config=llm_config,
-    system_message="You are a meta reviewer, you aggragate and review "
-    "the work of other reviewers and give a final suggestion on the content.",
-)
-
-def reflection_message(recipient, messages, sender, config):
-    return f'''Review the following content. 
-            \n\n {recipient.chat_messages_for_summary(sender)[-1]['content']}'''
-
-review_chats = [
-    {
-     "recipient": seo_reviewer,
-     "message": reflection_message,
-     "summary_method": "reflection_with_llm",
-     "summary_args": {"summary_prompt" :
-        "Return review into as JSON object only:"
-        "{'Reviewer': '', 'Review': ''}. Here Reviewer should be your role",},
-     "max_turns": 1},
-    {
-    "recipient": legal_reviewer, "message": reflection_message,
-     "summary_method": "reflection_with_llm",
-     "summary_args": {"summary_prompt" :
-        "Return review into as JSON object only:"
-        "{'Reviewer': '', 'Review': ''}.",},
-     "max_turns": 1},
-    {"recipient": ethics_reviewer, "message": reflection_message, 
-     "summary_method": "reflection_with_llm",
-     "summary_args": {"summary_prompt" :
-        "Return review into as JSON object only:"
-        "{'reviewer': '', 'review': ''}",},
-     "max_turns": 1},
-     {"recipient": meta_reviewer,
-      "message": "Aggregrate feedback from all reviewers and give final suggestions on the writing.",
-     "max_turns": 1},
-]
-
-critic.register_nested_chats(
-    review_chats,
-    trigger=writer,
-)
-
-res = critic.initiate_chat(
-    recipient=writer,
-    message=task,
-    max_turns=2,
-    summary_method="last_msg"
-)
+    # Get the piece name.
+    piece = board.piece_at(move.to_square)
+    piece_symbol = piece.unicode_symbol()
+    piece_name = (
+        chess.piece_name(piece.piece_type).capitalize()
+        if piece_symbol.isupper()
+        else chess.piece_name(piece.piece_type)
+    )
+    return f"Moved {piece_name} ({piece_symbol}) from "\
+    f"{chess.SQUARE_NAMES[move.from_square]} to "\
+    f"{chess.SQUARE_NAMES[move.to_square]}."
